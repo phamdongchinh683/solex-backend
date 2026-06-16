9411fe9f94589025bbafb8c9a53360a42e151dbf
pham dong chinh <dchinh6803@gmail.com>
2026-06-16 11:04:15 +0700

    ...

--- src/main/java/com/example/solex_backend/controller/HealthController.java
+++ src/main/java/com/example/solex_backend/controller/HealthController.java
@@ -3,9 +3,12 @@ package com.example.solex_backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.Map;
 
 @RestController
+@Tag(name ="health")
 public class HealthController {
 
     @GetMapping("/health")
